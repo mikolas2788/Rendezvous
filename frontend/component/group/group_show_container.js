74f0c2e63feb68933ac11e4ac90c0c66d4a8c874
Myron <myron.yook@gmail.com>
2019-04-01 13:13:00 -0400

    begin of group show

---
+++ frontend/component/group/group_show_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux'; 
+import GroupShow from './group_show'; 
+import { fetchGroup } from '../../action/group_actions'
+
+const msp = (state, ownProps) => {
+    return {
+        group: state.entities.groups[ownProps.match.params.groupId]
+    }
+} 
+
+const mdp = (dispatch) => {
+    return {
+        fetchGroup: (id) => dispatch(fetchGroup(id))
+    }
+} 
+
+export default connect (msp, mdp) (GroupShow); 
