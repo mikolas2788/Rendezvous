3d94eb6abf32746b0e8d492cc3a74b688f816ce4
Myron <myron.yook@gmail.com>
2019-04-01 21:29:19 -0400

    profile menu dropdown

---
+++ frontend/component/navbar/nav_bar_dropdown.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NavBarDropDown extends React.Component {
+
+    render () {
+        <div className='navbar-profile-icon'>
+            <div className='navbar-menu-button'>
+                <button 
+                    className='navbar-dropdown-button'
+                    onClick={}>
+                </button>
+            </div>
+        </div>
+    }
+}
+
+export default NavBarDropDown; 
