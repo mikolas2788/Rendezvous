553f1922a71c14bd3b43544b8f5ddfbb3dd81fb6
Myron <myron.yook@gmail.com>
2019-04-19 16:39:19 -0400

    event create form

---
+++ frontend/component/event/event_create_form.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom'; 
+
+class CreateEventForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            creator_id: this.props.creatorId, 
+            title: '',
+            start_time: '',
+            end_time: '',
+            description: '',
+            location: ''
+        }
+    }
+
+    render () {
+        return (
+            <div className='ec-strip'>
+                <div className='ec-strip-left'>
+                    <div className='ec-header'>
+                        <h1>Create an event</h1>
+                        <p>Group's Name goes here</p>
+                    </div>
+                </div>
+                <div className='ec-strip-right'>
+
+                </div>
+            </div>
+        )
+    }
+
+}
---
+++ frontend/component/event/event_create_form_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux'; 
+import { createEvent } from '../../action/event_actions';
+import EventCreateForm from './event_create_form'; 
+
+const msp = (state, ownProps) => {
+    return {
+        creatorId: state.session.id,
+        currentGroup: state.entities.groups[ownProps.match.params.groupId]
+    };
+};
+
+const mdp = (dispatch) => {
+    return {
+        createEvent: (event) => dispatch(createEvent(event))
+    };
+;}
+
+export default connect (msp, mdp) (EventCreateForm);
