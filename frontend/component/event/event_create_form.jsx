1cbaf8664acd1084621eb7da4683b7053ef2ec69
Myron <myron.yook@gmail.com>
2019-04-21 16:15:49 -0400

    event create inputs

--- frontend/component/event/event_create_form.jsx
+++ frontend/component/event/event_create_form.jsx
@@ -13,6 +13,23 @@ class CreateEventForm extends React.Component {
             description: '',
             location: ''
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this); 
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.createEvent(this.state).then((payload) => {
+            return (
+                this.props.history.push(`/groups/${payload.group.id}/events`)
+            );
+        });
+    }
+
+    handleUpdate(field) {
+        return (event) => {
+            this.setState({ [field]: event.target.value });
+        };
     }
 
     render () {
@@ -23,8 +40,51 @@ class CreateEventForm extends React.Component {
                         <h1>Create an event</h1>
                         <p>Group's Name goes here</p>
                     </div>
-                    <div>
-                    
+                    <div className='ec-form-strip'>
+                        <form className='ec-form' onSubmit={this.handleSubmit}>
+                            <div className='ec-inputs'>
+                                <div className='ec-input'>
+                                    <label>
+                                        Title
+                                        <input 
+                                            className='ec-input-box'
+                                            onChange={this.handleUpdate('location')}
+                                            type="text"
+                                            value={this.state.location}
+                                        />
+                                    </label>
+                                </div>
+                                <div className='ec-input'>
+                                    <label>
+                                        Date and Time
+                                        <input 
+                                            className='ec-input-box'
+                                            onChange={this.handleUpdate('start_time')}
+                                            type="date"
+                                            value={this.state.start_time}
+                                        />
+                                        <input 
+                                            className='ec-input-box'
+                                            onChange={this.handleUpdate('start_time')}
+                                            type="time"
+                                            value={this.state.start_time}
+                                        />
+                                    </label>
+                                </div>
+                                <div className='ec-input'>
+                                    <label>
+                                        Duration
+                                        <input 
+                                            className='ec-input-box'
+                                            onChange={this.handleUpdate('end_time')}
+                                            type="date"
+                                            value={this.state.end_time}
+                                        />
+                                    </label>
+                                </div>
+
+                            </div>
+                        </form>
                     </div>
                 </div>
                 <div className='ec-strip-right'>
@@ -33,5 +93,6 @@ class CreateEventForm extends React.Component {
             </div>
         )
     }
-
 }
+
+export default withRouter (CreateEventForm); 
