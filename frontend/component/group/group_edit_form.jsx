e8ab72f705af3c5641a4f7a6b321b4e621e0a6ad
Myron <myron.yook@gmail.com>
2019-05-02 14:43:09 -0400

    further styling of group edit

--- frontend/component/group/group_edit_form.jsx
+++ frontend/component/group/group_edit_form.jsx
@@ -103,91 +103,9 @@ class EditGroupForm extends React.Component {
                                     />
                             </div>
                         </div>
-
-
                     </form>
                 </div>
             </div>
-        //     <div className='cg-strip'>
-        //         <div className='cg-splash-strip'>
-        //             <div className='cg-splash'>
-        //                 <img
-        //                     className='cg-splash-banner'
-        //                     src='https://bit.ly/2UfQMny'
-        //                 />
-        //             </div>
-        //             <div className='cg-splash-content'>
-        //                 <h1>Edit your Rendezvous</h1>
-        //                 <h3>We'll help you find the right people to make it happen</h3>
-        //             </div>
-        //         </div>
-        //         <div className='cg-form-strip'>
-        //             <form className='cg-form' onSubmit={this.handleSubmit}>
-        //                 <div className='cg-inputs'>
-        //                     <div className='cg-input'>
-        //                         <img className='cg-icon' src='https://bit.ly/2JS4HvT' />
-        //                         <div>
-        //                             <p className='cg-step'>Step 1 of 4</p>
-        //                             <label>
-        //                                 What's your Rendezvous Group's hometown?
-        //                                 <input
-        //                                     className='cg-input-box'
-        //                                     onChange={this.handleUpdate('location')}
-        //                                     value={this.state.location}
-        //                                 />
-        //                             </label>
-        //                         </div>
-        //                     </div>
-        //                     <div className='cg-input'>
-        //                         <img className='cg-icon' src='https://bit.ly/2uE5q9A' />
-        //                         <div>
-        //                             <p className='cg-step'>Step 2 of 4</p>
-        //                             <label>
-        //                                 What will you change your Rendezvous name to?
-        //                                 <input
-        //                                     className='cg-input-box'
-        //                                     onChange={this.handleUpdate('title')}
-        //                                     value={this.state.title}
-        //                                 />
-        //                             </label>
-        //                         </div>
-        //                     </div>
-        //                     <div className='cg-input'>
-        //                         <img className='cg-icon' src='https://bit.ly/2UnZEaE' />
-        //                         <div>
-        //                             <p className='cg-step'>Step 3 of 4</p>
-        //                             <label>
-        //                                 Describe who should join, and what your Rendezvous will do.
-        //                                 <textarea
-        //                                     className='cg-input-big-box'
-        //                                     onChange={this.handleUpdate('about')}
-        //                                     value={this.state.about} />
-        //                             </label>
-        //                         </div>
-        //                     </div>
-        //                     <div className='cg-input'>
-        //                         <img className='cg-icon' src='https://bit.ly/2WAG1JQ' />
-        //                         <div>
-        //                             <p className='cg-step'>Step 4 of 4</p>
-        //                             <label>
-        //                                 What it means to be a Rendezvous.
-        //                                 <ul className='cg-rules'>
-        //                                     <li>Real, in person conversations</li>
-        //                                     <li>Open and honest intentions</li>
-        //                                     <li>Always safe and respectful</li>
-        //                                     <li>Put your members first</li>
-        //                                 </ul>
-        //                             </label>
-        //                             <input
-        //                                 className='cg-submit'
-        //                                 type="submit"
-        //                                 value='Edit Group' />
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </form>
-        //         </div>
-        //     </div>
         )
     }
 }
