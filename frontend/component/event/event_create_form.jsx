9c075bdcaf21cec3709c561dce18af3960986507
Myron <myron.yook@gmail.com>
2019-05-31 20:00:10 -0400

    initializing create events form

--- frontend/component/event/event_create_form.jsx
+++ frontend/component/event/event_create_form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DateTimePicker from 'react-datetime-picker'
+// import DateTimePicker from 'react-datetime-picker'
 // import DayPicker from 'react-day-picker';
 // import 'react-day-picker/lib/style.css';
 import { withRouter } from 'react-router-dom'; 
@@ -44,60 +44,20 @@ class CreateEventForm extends React.Component {
 
     render () {
         return (
-            <div className='ec-strip'>
-                <div className='ec-strip-left'>
-                    <div className='ec-header'>
-                        <h1>Create an event</h1>
-                        <p>Group's Name goes here</p>
-                    </div>
-                    <div className='ec-form-strip'>
-                        <form className='ec-form' onSubmit={this.handleSubmit}>
-                            <div className='ec-inputs'>
-                                <div className='ec-input'>
-                                    <label>
-                                        Title
-                                        <input 
-                                            className='ec-input-box'
-                                            onChange={this.handleUpdate('location')}
-                                            type="text"
-                                            value={this.state.location}
-                                        />
-                                    </label>
-                                </div>
-                                <div className='ec-input'>
-                                    <label>
-                                        Start
-                                        <DayPicker
-                                            start_time={this.state.start_time}
-                                            onDayClick={this.handleDayClick}
-                                        />
-                                        <input 
-                                            className='ec-input-box'
-                                            onChange={this.handleUpdate('start_time')}
-                                            type="time"
-                                            value={this.state.start_time}
-                                        />
-                                    </label>
-                                </div>
-                                <div className='ec-input'>
-                                    <label>
-                                        End
-                                        <DayPicker
-                                            end_time={this.state.end_time}
-                                            onDayClick={this.handleDayClick}
-                                        />
-                                    </label>
-                                </div>
+            <div className='ce-strip'>
+                <div className='ce-main'>
+                    <div className='ce-left'>
 
-                            </div>
-                        </form>
                     </div>
-                </div>
-                <div className='ec-strip-right'>
+                    <div className='ce-right'>
 
+                    </div>
+                    <div className='ce-footer'>
+
+                    </div>
                 </div>
             </div>
-        )
+        ) 
     }
 }
 
