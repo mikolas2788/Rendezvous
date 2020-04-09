3cd87447543ac3acbef256fa322aa3e854e71878
Myron <myron.yook@gmail.com>
2020-04-08 20:51:51 -0400

    work to main

--- frontend/component/main/main.jsx
+++ frontend/component/main/main.jsx
@@ -1,14 +1,34 @@
 import React from 'react'; 
+// import GroupIndex from '../group/group_index_container'
+import { connect } from 'react-redux';
+import GroupIndex from '../group/group_index_container';
+import { fetchGroups, deleteGroup } from '../../action/group_actions';
 
 class MainBody extends React.Component {
 
     render () {
         return (
             <div className='main-page-body'>
-                
+                <GroupIndex />
             </div>
         )
     }
 }
 
-export default MainBody; 
+// export default MainBody; 
+
+
+const msp = state => {
+    return ({
+        groups: Object.values(state.entities.groups)
+    });
+};
+
+const mdp = dispatch => {
+    return ({
+        fetchGroups: () => dispatch(fetchGroups()),
+        deleteGroup: (id) => dispatch(deleteGroup(id))
+    });
+};
+
+export default connect(msp, mdp)(MainBody);
