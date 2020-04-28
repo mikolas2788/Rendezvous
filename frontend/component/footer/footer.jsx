fde881358b6ba81ec6598405c3522bf18b69785d
Myron <myron.yook@gmail.com>
2020-04-27 21:03:52 -0400

    refactor to footer

--- frontend/component/footer/footer.jsx
+++ frontend/component/footer/footer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends React.Component {
  
