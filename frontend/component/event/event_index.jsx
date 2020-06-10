e2fefa81bd62582487c34d5ae6de771d670edf36
Myron <myron.yook@gmail.com>
2020-06-10 01:54:17 -0400

    created date formatter

--- frontend/component/event/event_index.jsx
+++ frontend/component/event/event_index.jsx
@@ -2,25 +2,31 @@ import React, { useState } from 'react'
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import { Calendar } from 'react-modern-calendar-datepicker';
 
-class EventIndex extends React.Component {
-
-    constructor (props) {
-        super (props) 
-
-    }
-
-    render () {
-
-        return (
-            <div className='eventindex-main'>
-                <h1> Available Events </h1>
-                <div className='eventindex-strip'>
-                    <Calendar />
+const EventIndex = () => {
+    let formattedDate = dateformatter(new Date())
+    const [ selectedDay, setSelectedDay ] = useState(formattedDate)
     
-                </div>
+    return (
+        <div className='eventindex-container'>
+            <div className='eventindex-left'>
+                <h1> {formattedDate} </h1>
             </div>
-        )
-    }
+            <div className='eventindex-right'>
+                <Calendar 
+                    // value={selectedDay}
+                    onChange={setSelectedDay}
+                    colorPrimary={`#00a2c7`}
+                />
+            </div>
+        </div>
+    )
+}
+
+function dateformatter(date) {
+    let splitDate = date.toDateString().split(" ")
+    let month = date.toLocaleString('default', { month: 'long' })
+    let [day, unused, num] = splitDate;
+    return day + ", " + month + " " + num
 }
 
 export default EventIndex
