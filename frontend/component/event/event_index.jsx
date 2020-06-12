db9819bec116edaf475eae124fdfa02c88f66938
Myron <myron.yook@gmail.com>
2020-06-12 18:09:20 -0400

    react hooks and date implementation for calendar

--- frontend/component/event/event_index.jsx
+++ frontend/component/event/event_index.jsx
@@ -1,32 +1,42 @@
 import React, { useState } from 'react'
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
-import { Calendar } from 'react-modern-calendar-datepicker';
+import { Calendar, utils } from 'react-modern-calendar-datepicker';
 
 const EventIndex = () => {
     let formattedDate = dateformatter(new Date())
-    const [ selectedDay, setSelectedDay ] = useState(formattedDate)
-    
+    let today = utils().getToday()
+    const [ selectedDay, setSelectedDay ] = useState(today)
+
+    function dateFormatter (selectedDay) {
+        return today.month + " " + today.day
+    }
+
+    // function handleChange () {
+    //     dateFormatter(selectedDay)
+    //     this.setSelectedDay()
+    // }
+
+    function dateformatter(date) {
+        let splitDate = date.toDateString().split(" ")
+        let month = date.toLocaleString('default', { month: 'long' })
+        let [day, unused, num] = splitDate;
+        return day + ", " + month + " " + num
+    }
+
     return (
         <div className='eventindex-container'>
             <div className='eventindex-left'>
-                <h1> {formattedDate} </h1>
+                <h1> { dateFormatter(selectedDay) } </h1>
             </div>
             <div className='eventindex-right'>
                 <Calendar 
-                    // value={selectedDay}
+                    value={selectedDay}
                     onChange={setSelectedDay}
-                    colorPrimary={`#00a2c7`}
+                    colorPrimary={'#00a2c7'}
                 />
             </div>
         </div>
     )
-}
-
-function dateformatter(date) {
-    let splitDate = date.toDateString().split(" ")
-    let month = date.toLocaleString('default', { month: 'long' })
-    let [day, unused, num] = splitDate;
-    return day + ", " + month + " " + num
 }
 
 export default EventIndex
