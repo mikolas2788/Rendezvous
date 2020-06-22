16fd441669b5e57302d8c2e611a3d1db5ceef099
Myron <myron.yook@gmail.com>
2020-06-21 22:12:52 -0400

    work to event create styling

--- frontend/component/event/event_create_form.jsx
+++ frontend/component/event/event_create_form.jsx
@@ -47,79 +47,85 @@ class CreateEventForm extends React.Component {
             <div className='ce-strip'>
                 <div className='ce-form-strip'>
                     <form className='ce-form' onSubmit={this.handleSubmit}>
-                        <div className='ce-left'>
-                            <div className='ce-header'>
-                                <h1>Create an Event</h1>
-                                <p>Group Name Goes Here</p>
+                        <div className='ce-main'>
+                            <div className='ce-left'>
+                                <div className='ce-header'>
+                                    <h1>Create an Event</h1>
+                                    <p>Group Name Goes Here</p>
+                                </div>
+                                <div className='ce-input'>
+                                    <h1>Title (required)</h1>
+                                    <input
+                                        className='ce-input-box'
+                                        onChange={this.handleUpdate('title')}
+                                        value={this.state.title}
+                                    />
+                                </div>
+                                <div className='ce-input'>
+                                    <h1>Date and Time</h1>
+                                    <input
+                                        className='ce-input-box'
+                                        onChange={this.handleUpdate('date')}
+                                        value={this.state.date}
+                                    />
+                                </div>
+                                <div className='ce-input'>
+                                    <h1>Duration</h1>
+                                    <input
+                                        className='ce-input-box'
+                                        onChange={this.handleUpdate('time')}
+                                        value={this.state.time}
+                                    />
+                                </div>
+                                <div className='ce-input'>
+                                    <h1>Description</h1>
+                                    <p>Let your attendees know what to expect, including the agenda, what they need to bring, and how to find the group.</p>
+                                    <textarea
+                                        className='ce-input-big-box'
+                                        onChange={this.handleUpdate('description')}
+                                        value={this.state.description}
+                                    />
+                                </div>
+                                <div className='ce-input'>
+                                    <h1>Location</h1>
+                                    <input
+                                        className='ce-input-box'
+                                        onChange={this.handleUpdate('location')}
+                                        value={this.state.location}
+                                    />
+                                </div>
                             </div>
-                            <div className='ce-input'>
-                                <h1>Title (required)</h1>
-                                <input
-                                    className='ce-input-box'
-                                    onChange={this.handleUpdate('title')}
-                                    value={this.state.title}
-                                />
-                            </div>
-                            <div className='ce-input'>
-                                <h1>Date and Time</h1>
-                                <input
-                                    className='ce-input-box'
-                                    onChange={this.handleUpdate('date')}
-                                    value={this.state.date}
-                                />
-                            </div>
-                            <div className='ce-input'>
-                                <h1>Duration</h1>
-                                <input
-                                    className='ce-input-box'
-                                    onChange={this.handleUpdate('time')}
-                                    value={this.state.time}
-                                />
-                            </div>
-                            <div className='ce-input'>
-                                <h1>Description</h1>
-                                <p>Let your attendees know what to expect, including the agenda, what they need to bring, and how to find the group.</p>
-                                <textarea
-                                    className='ce-input-big-box'
-                                    onChange={this.handleUpdate('description')}
-                                    value={this.state.description}
-                                />
-                            </div>
-                            <div className='ce-input'>
-                                <h1>Location</h1>
-                                <input
-                                    className='ce-input-box'
-                                    onChange={this.handleUpdate('location')}
-                                    value={this.state.location}
-                                />
-                            </div>
-                        </div>
-                        <div className='ce-right'>
-                            <div className='ce-tips'>
-                                <h1>
-                                    Tips for a great event
-                                </h1>
-                                <h3>
-                                    Be descriptive
-                                </h3>
-                                <p>
-                                    A good title immediately gives people an idea of what the event is about.
-                                </p>
-                                <h3>
-                                    Get organized
-                                </h3>
-                                <p>
-                                    Describe things in a clear order so it's easy to digest. Start with an overall description of the event and include a basic agenda, before you move into really specific details.
-                                </p>
-                                <h3>
-                                    Add an image
-                                </h3>
-                                <p>
-                                    Upload a photo or image to give members a better feel for the event.
-                                </p>
+                            <div className='ce-right'>
+                                <div className='ce-tips'>
+                                    <h1>
+                                        Tips for a great event
+                                    </h1>
+                                    <h3>
+                                        Be descriptive
+                                    </h3>
+                                    <p>
+                                        A good title immediately gives people an idea of what the event is about.
+                                    </p>
+                                    <h3>
+                                        Get organized
+                                    </h3>
+                                    <p>
+                                        Describe things in a clear order so it's easy to digest. Start with an overall description of the event and include a basic agenda, before you move into really specific details.
+                                    </p>
+                                    <h3>
+                                        Add an image
+                                    </h3>
+                                    <p>
+                                        Upload a photo or image to give members a better feel for the event.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                         <div className='ce-footer'>
+                            <input
+                                className='ce-submit'
+                                type="submit"
+                                value='Agree & Continue' />
                             <div className='ce-footer-left'>
 
                             </div>
